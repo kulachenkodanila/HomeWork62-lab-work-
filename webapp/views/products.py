eb3b076c6baa69f5b00557bfff85f4691e7ef1cb
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-08-23 15:00:31 +0300

    Добавил применение LoginRequiredMixin

--- webapp/views/products.py
+++ webapp/views/products.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import UpdateView, DeleteView, DetailView, CreateView
 
@@ -24,7 +25,7 @@ class ProductView(DetailView):
     queryset = Product.objects.filter(amount__gt=0)
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'product/product_create.html'
@@ -33,7 +34,7 @@ class ProductCreateView(CreateView):
         return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'product/product_update.html'
@@ -42,7 +43,7 @@ class ProductUpdateView(UpdateView):
         return reverse('webapp:product_view', kwargs={'pk': self.object.pk})
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'product/product_delete.html'
     success_url = reverse_lazy('webapp:index')
