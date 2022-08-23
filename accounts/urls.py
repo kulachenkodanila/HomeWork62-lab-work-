112a501b1f2ef3940e70314dde6e43592d464d8b
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-08-23 14:00:11 +0300

    Убрал старую модель (Profile), убрал ненужные импорты и файлы

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView, ProfileView, ChangeProfileView, ChangePasswordView
+from accounts.views import RegisterView
 
 app_name = "accounts"
 
@@ -9,7 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('registration/', RegisterView.as_view(), name="registration"),
-    path('<int:pk>/', ProfileView.as_view(), name="profile"),
-    path('<int:pk>/change/', ChangeProfileView.as_view(), name="change_profile"),
-    path('change/password/', ChangePasswordView.as_view(), name="change_password"),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,11 @@
 from django.contrib.auth import login, logout, authenticate, get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.contrib.auth.views import PasswordChangeView
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
+
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView
 
-from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm
-from accounts.models import Profile
+from accounts.forms import MyUserCreationForm
+
 
 User = get_user_model()
 
@@ -18,11 +15,11 @@ class RegisterView(CreateView):
     template_name = 'registration.html'
     form_class = MyUserCreationForm
 
-    def form_valid(self, form):
-        user = form.save()
-        Profile.objects.create(user=user)
-        login(self.request, user)
-        return redirect(self.get_success_url())
+    # def form_valid(self, form):
+    #     user = form.save()
+    #     self.objects.create(user=user)
+    #     login(self.request, user)
+    #     return redirect(self.get_success_url())
 
     def get_success_url(self):
         next_url = self.request.GET.get('next')
@@ -52,65 +49,4 @@ def logout_view(request):
     return redirect('webapp:index')
 
 
-class ProfileView(LoginRequiredMixin, DetailView):
-    model = User
-    template_name = "profile.html"
-    paginate_by = 5
-    paginate_orphans = 0
-    context_object_name = "user_obj"
-
-    def get_context_data(self, **kwargs):
-        paginator = Paginator(self.get_object().projects.all(),
-                              self.paginate_by,
-                              self.paginate_orphans)
-        page_number = self.request.GET.get('page', 1)
-        page_object = paginator.get_page(page_number)
-        context = super().get_context_data(**kwargs)
-        context['page_obj'] = page_object
-        context['projects'] = page_object.object_list
-        context['is_paginated'] = page_object.has_other_pages()
-        return context
-
-
-class ChangeProfileView(PermissionRequiredMixin, UpdateView):
-    model = User
-    form_class = UserChangeForm
-    template_name = "change_user.html"
-    profile_form_class = ProfileChangeForm
-    context_object_name = "user_obj"
-
-    def has_permission(self):
-        return self.request.user.is_superuser or self.request.user == self.get_object()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if "profile_form" not in context:
-            context["profile_form"] = self.profile_form_class(instance=self.get_object().profile)
-        return context
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        form = self.form_class(instance=self.object, data=request.POST)
-        profile_form = self.profile_form_class(instance=self.object.profile,
-                                               data=request.POST,
-                                               files=request.FILES)
-        if form.is_valid():
-            return self.form_valid(form, profile_form)
-        else:
-            return self.form_invalid(form, profile_form)
-
-    def form_valid(self, form, profile_form):
-        form.save()
-        profile_form.save()
-        return redirect("accounts:profile", self.object.pk)
-
-    def form_invalid(self, form, profile_form):
-        return self.render_to_response(self.get_context_data(form=form, profile_form=profile_form))
-
-
-class ChangePasswordView(PasswordChangeView):
-    template_name = "change_password.html"
-
-    def get_success_url(self):
-        return reverse("accounts:profile", kwargs={"pk": self.request.user.pk})
 
